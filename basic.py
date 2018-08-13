d03e8bacb98f1b0c4e33bcae0ae67e73d2f25a00
deplax <sturdyegg@gmail.com>
2018-08-13 17:30:58 +0900

    [feat]scope

--- basic.py
+++ basic.py
@@ -1,22 +1,26 @@
 # coding=utf-8
 
+global_value = "GLOBAL!"
+
 
 def has_key_vs_in():
+    print("=== has_key vs in ===")
+
     #  has_key 말고 in 쓰세요. 이게 더 직관적입니다.
     #  그리고 파이썬3에서 has_key 지원 안해요.
     dic = {"korea": 82, "japan": 81}
 
-    print("=== has_key vs in ===")
     # print(dic.has_key("korea"))
     print("korea" in dic)
     print("corea" in dic)
 
 
 def true_false():
+    print("=== boolean operation ===")
+
     # boolean 자료형은 operation 없이 사용하세요.
     true_value = True
 
-    print("=== boolean operation ===")
     # good!
     if true_value:
         print("no operation")
@@ -28,10 +32,38 @@ def true_false():
         print("is operation")
 
 
+def scope():
+    print("=== scope ===")
+    # local -> enclosed -> global -> built-in
+    # LEGB rule
+    # 위 순서대로 검색합니다.
+
+    def out_func():
+        non_local = "NONLOCAL!"
+
+        def in_func():
+
+            # 아래 두줄이 없으면 로컬변수로 찾고 선언되지 않았으니 애러 출력.
+            global global_value
+            nonlocal non_local      #nonlocal은 파이썬3 에만 있다.
+
+            global_value += "!"
+            non_local += "!"
+
+            local_value = "LOCAL!"
+            return ("%s %s %s" % (local_value, non_local, global_value))
+
+        return in_func()
+
+    print(out_func())
+
+
 def main():
     has_key_vs_in()
     print()
     true_false()
+    print()
+    scope()
 
 
 if __name__ == "__main__":
