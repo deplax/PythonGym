b5d80cb9321992c19e749c4c57422378516ee4a6
deplax <sturdyegg@gmail.com>
2018-08-13 17:13:05 +0900

    [feat]여러분 in을 쓰세요.

---
+++ basic.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+
+def main():
+    #  has_key 말고 in 쓰세요. 이게 더 직관적입니다.
+    #  그리고 파이썬3에서 has_key 지원 안해요.
+    dic = {"korea": 82, "japan": 81}
+    print("=== has_key vs in ===")
+    # print(dic.has_key("korea"))
+    print("korea" in dic)
+
+
+if __name__ == "__main__":
+    main()
